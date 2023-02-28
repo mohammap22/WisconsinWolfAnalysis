8e9795091e9daa028746f2ecd7cf741b9613298d
Emily Rolen <ekrolen@uw.edu>
2023-02-28 10:28:36 -0800

    Added stats module and test file for stats module

---
+++ WisconsinWolfAnalysis/stats_module.py
@@ -0,0 +1,15 @@
+"""PROGRAM DOCSTRING GOES HERE"""
+
+def hypothesis_function_one():
+    """MODULE DOCSTRING GOES HERE"""
+    return "Nothing written yet"
+
+
+def hypothesis_function_two():
+    """MODULE DOCSTRING GOES HERE"""
+    return "Nothing written yet"
+
+
+def hypothesis_function_three():
+    """MODULE DOCSTRING GOES HERE"""
+    return "Nothing written yet"
---
+++ WisconsinWolfAnalysis/test_stats_module.py
@@ -0,0 +1,8 @@
+"""MODULE DOCSTRING GOES HERE"""
+
+import unittest
+
+class UnitTests(unittest.TestCase):
+
+    def dummy_tester(self):
+        return "Nothing written yet"
