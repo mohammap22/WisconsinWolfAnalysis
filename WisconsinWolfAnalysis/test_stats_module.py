21841f09a41b5ceefb2ac7b1a79c6f00d99a9acb
Emily Rolen <ekrolen@uw.edu>
2023-02-28 15:34:28 -0800

    Building out more test cases and stats module

--- WisconsinWolfAnalysis/test_stats_module.py
+++ WisconsinWolfAnalysis/test_stats_module.py
@@ -11,17 +11,17 @@ class UnitTests(unittest.TestCase):
     This class also relies on the unittest module and TestCase class
     """
 
-    @mock.patch('hypothesis_function_one.input', create = True)
+    @mock.patch('stats_module.input', create = True)
     def test_correlation_file_format(self, mocked_input):
         """This test verifies the file input into the correlation
         function is a csv"""
-        mocked_input = 'pdf/WolfReport2017.pdf'
+        mocked_input = './pdf/WolfReport2017.pdf'
         with self.assertRaises(TypeError):
             hypothesis_function_one()
 
     def test_correlation_three_columns(self, mocked_input):
         """This test verifies that the dataframe has 3 columns"""
-        mocked_input.side_effect = ['pdf/test_files/two_col_wolf_and_deer_pop.csv']
+        mocked_input = 'pdf/test_files/two_col_wolf_and_deer_pop.csv'
         with self.assertRaises(TypeError):
             hypothesis_function_one()
 
@@ -36,10 +36,25 @@ class UnitTests(unittest.TestCase):
     def test_correlation_enough_entries(self, mocked_input):
         """This test verifies that the user has at least 2 rows in
         their data frame"""
-        mocked_input = 'pdf/test_files/wolf_and_deer_pop_one_row.csv'
         with self.assertRaises(TypeError):
+            mocked_input = 'pdf/test_files/wolf_and_deer_pop_one_row.csv'
+            hypothesis_function_one()
+    
+    def test_correlation_no_nulls(self, mocked_input):
+        """This test verifies that no values in the data frame are
+        null"""
+        mocked_input = 'pdf/test_files/wolf_and_deer_pop_nulls.csv'
+        with self.assertRaises(ValueError):
             hypothesis_function_one()
 
+    def test_correlation_of_one(self, mocked_input):
+        """This test verifies that we get a pearsons correlation of 
+        1 given exactly the same data in x and y
+        
+        THIS TEST LIKELY NEEDS TO BE UPDATED AS I UNDERSTAND HOW
+        PEARSONR WORKS"""
+        mocked_input = 'pdf/test_files/wolf_and_deer_pop_one.csv'
+        self.assertAlmostEqual(hypothesis_function_one(),1)
 
 
 if __name__ == '__main__':
